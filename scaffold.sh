2bd9136a00d6f01b9842969a646148e61edc4cc5
Antoine Dray <antoine.dray@3ie.fr>
2019-02-13 14:42:52 +0100

    Adds scaffolding build script

---
+++ scaffold.sh
@@ -0,0 +1,2 @@
+cd TodoApi
+dotnet ef dbcontext scaffold "Host=localhost;Database=antoine.dray;Username=antoine.dray;Password=password" Npgsql.EntityFrameworkCore.PostgreSQL -d -f -c DataContext -o Models --verbose
