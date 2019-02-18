28971884bfa57791920c8b4358bd7a4d274fd1c4
Antoine Dray <antoine.dray@epita.fr>
2019-02-18 13:02:18 +0100

    Adds User service

---
+++ TodoApi/BusinessManagement/UserService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+using TodoApi.Dbo;
+
+namespace TodoApi.BusinessManagement
+{
+    public interface IUserService
+    {
+        Users Authenticate(string username, string password);
+    }
+
+    public class UserService : IUserService
+    {
+        // users hardcoded for simplicity, store in a db with hashed passwords in production applications
+        private List<Users> _users = new List<Users>
+        {
+            new Users { Id = 1, Username = "test", Password = "test" }
+        };
+
+        private readonly AppSettings _appSettings;
+        private readonly DataContext _context;
+
+        public UserService(IOptions<AppSettings> appSettings, DataContext context)
+        {
+            _appSettings = appSettings.Value;
+            _context = context;
+        }
+
+        public Users Authenticate(string username, string password)
+        {
+            var user = _users.SingleOrDefault(x => x.Username == username && x.Password == password);
+
+            // return null if user not found
+            if (user == null)
+            {
+                Console.WriteLine("Error here");
+                return null;
+            }
+
+
+            // authentication successful so generate jwt token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            user.Token = tokenHandler.WriteToken(token);
+
+            // remove password before returning
+            user.Password = null;
+
+            return user;
+        }
+    }
+}
