d1b61cfe5da63fac44034b93ee0fd7dd5b847043
Antoine Dray <antoine.dray@epita.fr>
2019-02-18 12:02:36 +0100

    Updates scaffold script to support differents envs

--- Build/scaffold.sh
+++ Build/scaffold.sh
@@ -1,2 +1,14 @@
 cd ../TodoApi
-dotnet ef dbcontext scaffold "Host=localhost;Database=antoine.dray;Username=antoine.dray;Password=password" Npgsql.EntityFrameworkCore.PostgreSQL -d -f -c DataContext -o Models --verbose
+
+if [ "$1" = "--3ie" ]; then
+    DB="antoine.dray"
+    USER="antoine.dray"
+elif [[ "$1" = "--Home" ]]; then
+    DB="todo"
+    USER="antoinedray"
+else
+    echo "Usage: ./scaffold.sh [env]"
+    exit 1
+fi
+
+dotnet ef dbcontext scaffold "Host=localhost;Database=${DB};Username=${USER};Password=password" Npgsql.EntityFrameworkCore.PostgreSQL -d -f -c DataContext -o Models --verbose
