9cc74faf98b77ea018c33cf72787c43ed0d79e57
Antoine Dray <antoine.dray@epita.fr>
2019-02-25 11:59:18 +0100

    Create test file for TodoItems

---
+++ TodoApiTests/TestTodoItems.cs
@@ -0,0 +1,34 @@
+﻿using Xunit;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+//using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+using TodoApi.DataAccess;
+using TodoApi.BusinessManagement;
+
+namespace TodoApiTests
+{
+    public class TestTodoItems
+    {
+        private DataContext GetContext()
+        {
+            var options = new DbContextOptionsBuilder<DataContext>()
+                              .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                              .Options;
+            var context = new DataContext(options);
+            return context;
+        }
+
+        public async Task<DataContext> Add_test_user()
+        {
+            var context = GetContext();
+            var access = new RepositoryUser(context);
+            var bm = new UserServices(access);
+            await bm.Create(new Users { Username = "user1" }, "test");
+            return context;
+        }
+    }
+}
